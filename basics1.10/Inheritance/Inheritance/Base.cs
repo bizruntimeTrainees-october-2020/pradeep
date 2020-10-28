e6eeeb54e8aef898c4cea4ca96cce8cfefb4fcc8
Pradeep kumar Bhoi <pradeepku.bhoi@gmail.com>
2020-10-28 17:03:20 +0530

    basics c#

---
+++ basics1.10/Inheritance/Inheritance/Base.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Base
+    {
+      
+        public Base()
+        {
+            Console.WriteLine("base constructor");
+        }
+       
+       public void apple()
+        {
+            Console.WriteLine("apple is green");
+        }
+        public void mango()
+        {
+            Console.WriteLine("mango is yellow");
+        }
+        public void lemon()
+        {
+            Console.WriteLine("lemon is sour");
+        }
+    }
+}
---
+++ basics1.10/Inheritance/Inheritance/Child.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Child:Base
+    {
+        Child()
+        {
+            Console.WriteLine("cherry constructor"); 
+        }
+        public void cherry()
+        {
+            Console.WriteLine("cherry is red");
+        }
+        static void Main()
+        {
+            //Child obj = new Child(60);
+            //obj.apple();
+            //obj.mango();
+            //obj.cherry();
+            Base v;
+            Child c1 = new Child();
+            v = c1;
+            v.lemon();
+            v.apple();
+            v.mango();
+            Console.ReadLine();
+        }
+    }
+}
