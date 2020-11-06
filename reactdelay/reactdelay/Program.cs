59791324749a32730417a77713cc9464cd8bc060
Pradeep kumar Bhoi <pradeepku.bhoi@gmail.com>
2020-11-06 16:16:53 +0530

    delayoperator

---
+++ reactdelay/reactdelay/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace reactdelay
+{
+    class Delay_Simple
+    {
+        static void Main()
+        {
+            var oneNumberEveryFiveSeconds = Observable.Interval(TimeSpan.FromSeconds(5));
+
+            // Instant echo
+            oneNumberEveryFiveSeconds.Subscribe(num =>
+            {
+                Console.WriteLine(num);
+            });
+
+            // One second delay
+            oneNumberEveryFiveSeconds.Delay(TimeSpan.FromSeconds(1)).Subscribe(num =>
+            {
+                Console.WriteLine("...{0}...", num);
+            });
+
+            // Two second delay
+            oneNumberEveryFiveSeconds.Delay(TimeSpan.FromSeconds(2)).Subscribe(num =>
+            {
+                Console.WriteLine("......{0}......", num);
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
+
