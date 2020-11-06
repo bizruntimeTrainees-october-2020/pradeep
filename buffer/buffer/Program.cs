a3cf4d83361959b9586f38079f18b002944f1f7f
Pradeep kumar Bhoi <pradeepku.bhoi@gmail.com>
2020-11-06 16:26:28 +0530

    bufferoperator

---
+++ buffer/buffer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Reactive.Linq;
+using System.Linq;
+namespace buffer
+{
+    class Program
+    {
+        static IEnumerable<string> EndlessBarrageOfEmail()
+        {
+            var random = new Random();
+            var emails = new List<String> { "Here is an email!", "Another email!", "Yet another email!" };
+            for (; ; )
+            {
+                // Return some random emails at random intervals.
+                yield return emails[random.Next(emails.Count)];
+                Thread.Sleep(random.Next(1000));
+            }
+        }
+        public static void Main(string[] args)
+        {
+           
+            var myInbox = EndlessBarrageOfEmail().ToObservable();
+
+            // Instead of making you wait 5 minutes, we will just check every three seconds instead. :)
+            var getMailEveryThreeSeconds = myInbox.Buffer(TimeSpan.FromSeconds(3)); //  Was .BufferWithTime(...
+
+            getMailEveryThreeSeconds.Subscribe(emails =>
+            {
+                Console.WriteLine("You've got {0} new messages!  Here they are!", emails.Count());
+                foreach (var email in emails)
+                {
+                    Console.WriteLine("> {0}", email);
+                }
+                Console.WriteLine();
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
