74c0d1901005f1740316e03fe6070c2b1509344d
Pradeep kumar Bhoi <pradeepku.bhoi@gmail.com>
2020-11-08 10:03:28 +0530

    socketprograming

---
+++ client/client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("This is Client");
+                Socket cilentside = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); //Establishing Connection
+                IPEndPoint iPEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888); // spcifies a port to initilize
+                cilentside.Connect(iPEnd); //accpets  new connections
+
+
+                string sendData = "";
+                do
+                {
+                    Console.Write("message :");
+                    sendData = Console.ReadLine();
+                    cilentside.Send(System.Text.Encoding.UTF8.GetBytes(sendData)); //decodes
+
+                }
+                while (sendData.Length > 0);
+                {
+                    cilentside.Close();
+                }
+
+             
+        }
+    }
+}
---
+++ server1/server1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace server1
+{
+    class Program
+    {
+        publicstatic void Main(string[] args)
+
+        {
+            Console.WriteLine("hello this is server");
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint iPEnd = new IPEndPoint((IPAddress.Parse("127.0.0.1")), 8888);
+            listenerSocket.Bind(iPEnd);
+            listenerSocket.Listen(2);
+            Console.WriteLine("waiting for client to connect");
+            Socket clientSocket = listenerSocket.Accept();
+            byte[] Buffer = new byte[clientSocket.SendBufferSize];
+            int readByte;
+            do
+            {
+                readByte = clientSocket.Receive(Buffer);
+                byte[] rData = new byte[readByte];
+                Array.Copy(Buffer, rData, readByte);
+                Console.WriteLine("the message: " + System.Text.Encoding.UTF8.GetString(rData));
+
+            }
+            while (readByte > 0);
+
+
+            Console.WriteLine("Client Disconnected");
+            Console.ReadKey();
+        }
+    }
+}
