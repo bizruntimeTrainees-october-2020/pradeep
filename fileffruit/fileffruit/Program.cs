f3e8511fe2cb587b02a03fc1407e5e0b762c8024
Pradeep kumar Bhoi <pradeepku.bhoi@gmail.com>
2020-11-08 09:46:55 +0530

    file

---
+++ fileffruit/fileffruit/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace fileffruit
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                TextWriter tw = new StreamWriter(@"E:\crayon.txt", true);
+    
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+           
+            string filepath = @"E:\crayon.txt";
+            List<string> lines = new List<string>();
+
+            lines.Add("oragnge");
+            lines.Add("blue");
+            lines.Add("green");
+            lines.Add("yellow");
+            File.WriteAllLines(filepath, lines);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+                Console.ReadLine();
+            
+        }
+    }
+}
